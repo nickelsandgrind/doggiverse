f4ac37d3cd8bc31c607c0656184f6cd5d9f1744e
LaBlanca Ponder <lablancaponder@gmail.com>
2018-04-24 17:15:18 -0600

    accept tokens, validate users and access protected routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load user model
 const User = require("../../models/User");
@@ -93,6 +94,19 @@ router.post("/login", (req, res) => {
   });
 });
 
-module.exports = router;
+// @route   GET api/users/current
+// @desc    Return current user - whoever the token belongs to
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
-// creating jason web token
+module.exports = router;
