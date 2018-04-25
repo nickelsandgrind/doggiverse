9569bb5aaab9f23114293e1d53b3c6ed7c41edd4
LaBlanca Ponder <lablancaponder@gmail.com>
2018-04-24 22:32:11 -0600

    created validation rules for registration and login

---
+++ validation/is-empty.js
@@ -0,0 +1,10 @@
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
+
+// this will check for undefined, null and emptry values
+// validator isEmpty will only check for an empty string
---
+++ validation/login.js
@@ -0,0 +1,27 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors, // would also be errors:errors so we only need one
+    isValid: isEmpty(errors) // since errors is an object and not a string, validator wont work so create a global function called isEmpty.
+    // could use the isempty method from lodash but want to minimize libraries used
+  };
+};
