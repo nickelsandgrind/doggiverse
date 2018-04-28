18a0d715af48224608f09bb538068a6c57f05343
LaBlanca Ponder <lablancaponder@gmail.com>
2018-04-28 14:11:16 -0600

    added create react app and added some start scripts

--- models/Profile.js
+++ models/Profile.js
@@ -2,41 +2,99 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
+// const ProfileSchema = new Schema({
+//   user: {
+//     type: Schema.Types.ObjectId,
+//     ref: "users"
+//   },
+//   handle: {
+//     type: String,
+//     required: true,
+//     max: 40
+//   },
+//   location: {
+//     type: String
+//   },
+//   birthday: {
+//     type: Date,
+//     required: true
+//   },
+//   numberofdogs: {
+//     type: String
+//   },
+//   tagline: {
+//     type: String
+//   },
+//   bio: {
+//     type: String
+//   },
+//   instagramUsername: {
+//     type: String
+//   },
+
+//   dog: [
+//     {
+//       name: {
+//         type: String,
+//         required: true
+//       },
+//       age: {
+//         type: String,
+//         required: true
+//       },
+//       breed: {
+//         type: String,
+//         required: true
+//       },
+//       color: {
+//         type: String,
+//         required: true
+//       },
+//       gender: {
+//         type: String,
+//         required: true
+//       },
+//       birthdate: {
+//         type: Date,
+//         required: true
+//       },
+//       loves: {
+//         type: String,
+//         required: true
+//       },
+//       dislikes: {
+//         type: String,
+//         required: true
+//       },
+//       favTreats: {
+//         type: String,
+//         required: true
+//       },
+//       allergies: {
+//         type: String
+//       },
+//       vaccinations: {
+//         type: String
+//       }
+//     }
+//   ],
+//     dogparks: [
+//     {
+//       name: {
+//         type: String,
+//         required: true
+//       },
+//       wastedisposal: {
+//         type: Boolean,
+//         default: false
+//       },
+//       description: {
+//         type: String
+//       }
+//     }
+//   ],
+// Create Schema
 const ProfileSchema = new Schema({
-  /// for the dogs
-  // user: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "users"
-  // },
-  // handle: {
-  //   type: String,
-  //   required: true,
-  //   max: 40
-  // },
-  // petName: {
-  //   type: [String]
-  // },
-  // breed: {
-  //   type: [String]
-
-  // },
-  // location: {
-  //   type: String
-  // },
-  // birthday: {
-  //   type: String,
-  //   required: true
-  // },
-  // tagline: {
-  //   type: String
-  // },
-  // bio: {
-  //   type: String
-  // },
-  // instagramUsername: {
-  //   type: String
-  // }
-
   user: {
     type: Schema.Types.ObjectId,
     ref: "users"
