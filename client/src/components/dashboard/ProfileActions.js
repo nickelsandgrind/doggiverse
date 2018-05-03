bcc185b82b16219340cbb994c5e070e91a4fdd62
LaBlanca Ponder <lablancaponder@gmail.com>
2018-05-02 18:50:46 -0600

    added ability to delete account

---
+++ client/src/components/dashboard/ProfileActions.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProfileActions = () => {
+  return (
+    <div className="btn-group mb-4" role="group">
+      <Link to="/edit-profile" className="btn btn-primary btn-lg mr-3">
+        <i className="fas fa-user-edit text-light mr-3" /> Edit Your Profile
+      </Link>
+      <Link to="/add-dog" className="btn btn-primary btn-lg mr-3">
+        <i className="fas fa-paw text-light mr-3" />
+        Add A Dog
+      </Link>
+    </div>
+  );
+};
+
+export default ProfileActions;
