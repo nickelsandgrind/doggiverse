6fdb3f342332fba4450d119c35ea32e2d31acd62
nickelsandgrind <38905624+nickelsandgrind@users.noreply.github.com>
2018-05-07 13:08:13 -0600

    footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => {
   return (
-    <footer className="bg-light text-grey mt-5 p-4 text-center">
+        <footer className="bg-primary footer text-white mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} Doggiverse | A social network
       for dog owners
     </footer>
